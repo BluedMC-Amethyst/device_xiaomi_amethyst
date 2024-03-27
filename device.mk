a1c71b3d3480bcb4d88edaf4a8203ee188a1a37f
Michael Bestas <mkbestas@lineageos.org>
2024-03-28 00:06:56 +0200

    amethyst: overlay: Enable battery cycle count
    Change-Id: I92bc8363e1c9e2ad54c29a0a30dce992e25f697d

--- device.mk
+++ device.mk
@@ -247,6 +247,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayAmethyst \
     FrameworksOverlayAmethyst \
+    LineageSettingsOverlayAmethyst \
     NfcOverlayAmethyst \
     SecureElementOverlayAmethyst \
     SettingsOverlayAmethyst \
