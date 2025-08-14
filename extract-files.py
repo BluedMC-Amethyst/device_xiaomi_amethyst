1bc4c4e4d80d9b0040a835e116fcd9c13e2033b2
Michael Bestas <mikeioannina@gmail.com>
2025-08-14 18:29:11 +0530

    amethyst: Shim pnscr binary with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -301,6 +301,9 @@ blob_fixups: blob_fixups_user_type = {
             'libaudioroute.so',
             'libaudioroute-v34.so'
     ),
+
+    'vendor/bin/pnscr': blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 
