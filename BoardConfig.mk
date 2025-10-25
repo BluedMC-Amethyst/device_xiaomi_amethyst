94841b3d7fb1fe91eb6a1ad142de6dbca3e36af7
Mikazuuu07 <mikazuuu07@luminedroid.org>
2025-10-25 19:57:50 +0000

    amethyst: Define Architecture flags
    Change-Id: I66c165ad385c84acea67732c71bc712ae6db867c
    Signed-off-by: Mikazuuu07 <mikazuuu07@luminedroid.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,12 @@
 
 DEVICE_PATH := device/xiaomi/amethyst
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a-branchprot
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
+
 # Inherit the proprietary files
 include vendor/xiaomi/amethyst/BoardConfigVendor.mk
