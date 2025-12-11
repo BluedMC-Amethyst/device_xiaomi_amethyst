1c59be4e147ea491269a434589c52a0b4fc09915
Mikazuuu07 <mikazuuu07@luminedroid.org>
2025-12-11 11:38:33 +0100

    amethyst: Generate compatibility matrix devices
    Change-Id: I4147ed538c40e157ef5da87a1d10361ca25b75ed
    Signed-off-by: Mikazuuu07 <mikazuuu07@luminedroid.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/compatibility_matrix.device.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
