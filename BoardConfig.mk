17320eb08b7b560bd56eba54058b6382e3e453fb
Mikazuuu07 <mikazuuu07@luminedroid.org>
2026-03-20 16:24:53 +0000

    amethyst: Import properties
    Change-Id: I1b60df9c982fa350d6a8190acb147f0d37ca0e15
    Signed-off-by: Mikazuuu07 <mikazuuu07@luminedroid.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,13 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := volcano
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
+
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
