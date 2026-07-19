99f72965e5b469f810d713174cf53a35a7fd00ea
Bluekyeet <bluekyeet@bluedmc.xyz>
2026-07-19 21:58:16 +1200

    amethyst: miuicamera stuff

--- device.mk
+++ device.mk
@@ -260,6 +260,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/amethyst-miuicamera/device.mk)
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
