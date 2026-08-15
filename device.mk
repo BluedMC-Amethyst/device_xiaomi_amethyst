711b21897647b8a635f57be2427ec3eb0d9c1203
Bluekyeet <bluekyeet@bluedmc.xyz>
2026-08-15 15:28:26 +1200

    amethyst: Add sensor notifier service and related implementations

--- device.mk
+++ device.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
+    sensor-notifier \
     sensors.dynamic_sensor_hal \
     sensors.xiaomi.v2
 
